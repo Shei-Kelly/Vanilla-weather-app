c2ee963765fb38e4c6109a5f1cb17bba15f8e929
shei-kelly <sheikelly920@gmail.com>
2022-02-17 11:29:31 +0100

    showing the current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("city");
+  let descriptionElement = document.querySelector("description");
+  let humidityElement = document.querySelector("humidity");
+  let windElement = document.querySelector("wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+let apikey = "a9acfb841e7c19f9624e7d8ba9d5fb29";
+let apiurl =
+  "https://api.openweathermap.org/data/2.5/weather?q=Bamenda&appid = ${apiKey}&units = metric";
 
+axios.get(apiUrl).then(displayTemperature);
