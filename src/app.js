c412491f4ffbaa344fc4d89e64f7a004380e19fa
shei-kelly <sheikelly920@gmail.com>
2022-02-21 16:04:53 +0100

    Initial commit

--- src/app.js
+++ src/app.js
@@ -1,13 +1,29 @@
 function formatDate(timestamp) {
   let Date = newDate(timestamp);
   let hours = date.getHours();
+  if (hours < 10) {
+    hours = "0${hours}";
+  }
   let minutes = date.getMinutes();
-  let day = date.getDay();
-  return '${day} ${hours}:${minutes}';
+  if (minutes < 10) {
+    minutes = "0${minutes}";
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return "${day} ${hours}:${minutes}";
   return "monday 1:47";
 }
 function displayTemperature(response) {
   console.log(response.data);
+
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
@@ -19,9 +35,11 @@ function displayTemperature(response) {
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
-  dateElement.innerHTML = formatDate(response.data.dt*1000);
-let apikey = "a9acfb841e7c19f9624e7d8ba9d5fb29";
-let apiurl =
-  "https://api.openweathermap.org/data/2.5/weather?q=Bamenda&appid = ${apiKey}&units = metric";
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+}
+let apiKey = "9acfb841e7c19f9624e7d8ba9d5fb29";
+
+let city = "Bamenda";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
