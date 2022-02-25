356ed0012814036613dd67910b084fa914549a13
shei-kelly <sheikelly920@gmail.com>
2022-02-25 11:12:23 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -69,7 +69,7 @@ form.addEventListener("submit", search);
 
 function displayCelsiusTemperature(event) {
   event.preventDefault();
-  celsiusLink.classList.add("active");
+  celsiustLink.classList.add("active");
   fahrenheitLink.classList.remove("active");
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
